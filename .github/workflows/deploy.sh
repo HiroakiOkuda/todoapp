29801b54af8d28de0461f19e03f2422e29d4cf4a
奥田 啓皓 <usr0104756@jinn1050.gmo.local>
2023-05-02 11:11:32 +0900

    fix: dockerで動かす

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,5 +8,4 @@ echo "$CONOHA_VPS_SSH_KEY" > ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
 
 # SSHでVPSに接続してコードをアップロード
-ssh -oStrictHostKeyChecking=no -i ~/.ssh/deploy_key $CONOHA_VPS_USER@$CONOHA_VPS_HOST "rm -rf $CONOHA_VPS_DIR/*"
-rsync -avz -e "ssh -oStrictHostKeyChecking=no -i ~/.ssh/deploy_key" --exclude='.git' ./ $CONOHA_VPS_USER@$CONOHA_VPS_HOST:$CONOHA_VPS_DIR --delete
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/deploy_key $CONOHA_VPS_USER@$CONOHA_VPS_HOST "cd $CONOHA_VPS_DIR && docker-compose pull && docker-compose down && docker-compose up -d"
