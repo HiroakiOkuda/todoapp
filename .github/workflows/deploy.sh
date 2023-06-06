ffaf368018d97eee46425b4c5e4446567e3fe031
奥田 啓皓 <usr0104756@jinn1050.gmo.local>
2023-06-06 14:05:52 +0900

    fix: update env variables

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,19 +4,19 @@ set -e
 
 # 変数のdebug
 
-echo "key: $key"
-echo "username: $username"
-echo "host: $host"
-echo "port: $port"
+echo "key: $KEY"
+echo "username: $USERNAME"
+echo "host: $HOST"
+echo "port: $PORT"
 
 # Conoha VPSへSSH接続するキーを作成
 mkdir -p ~/.ssh
-echo "$key" > ~/.ssh/deploy_key
+echo "$KEY" > ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
 
 # ローカルでDockerイメージをビルドしてリモートホストへ転送
 docker build -t todoapp-client .
-docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key $username@$host 'gunzip | docker load'
+docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key $USERNAME@$HOST 'gunzip | docker load'
 
 # SSHで接続して、Dockerコンテナを実行する
-ssh -i ~/.ssh/deploy_key $username@$host "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p $port:80 todoapp-client"
+ssh -i ~/.ssh/deploy_key $USERNAME@$HOST "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p $PORT:80 todoapp-client"
