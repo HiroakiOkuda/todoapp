e7d2d380deca040f550fcb15e682ec4f5ff34812
奥田 啓皓 <usr0104756@jinn1050.gmo.local>
2023-06-06 13:23:19 +0900

    fix: update script

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,12 +4,12 @@ set -e
 
 # Conoha VPSへSSH接続するキーを作成
 mkdir -p ~/.ssh
-echo "$CONOHA_VPS_SSH_KEY" > ~/.ssh/deploy_key
+echo "$key" > ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
 
 # ローカルでDockerイメージをビルドしてリモートホストへ転送
 docker build -t todoapp-client .
-docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key $CONOHA_VPS_USER@$CONOHA_VPS_HOST 'gunzip | docker load'
+docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key $username@$host 'gunzip | docker load'
 
 # SSHで接続して、Dockerコンテナを実行する
-ssh -i ~/.ssh/deploy_key $CONOHA_VPS_USER@$CONOHA_VPS_HOST "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p $APP_PORT:80 todoapp-client"
+ssh -i ~/.ssh/deploy_key $username@$host "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p $port:80 todoapp-client"
