12f2d46993cbe02e0345cd375b088e5d7bdf7028
奥田 啓皓 <usr0104756@jinn1050.gmo.local>
2023-06-13 16:06:24 +0900

    fix: update shellscript

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,18 +2,24 @@ bash
 #!/bin/bash
 set -e
 
+# GitHub Secretsから必要なシークレットキー情報を取得
+KEY=$KEY
+HOST=$HOST
+USERNAME=$USERNAME
+PORT=$PORT
+
 # Conoha VPSへSSH接続するキーを作成
 mkdir -p ~/.ssh
 ssh-keyscan -p ${PORT} ${HOST} >> ~/.ssh/known_hosts
-echo "${KEY}" > ~/.ssh/deploy_key
+echo "${KEY}" >> ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
 
 # 公開鍵をリモートサーバーのauthorized_keysに追加
-cat ~/.ssh/z | ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/z ${USERNAME}@${HOST} -p ${PORT} 'mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
+ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy_key ${USERNAME}@${HOST} -p ${PORT} "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
 
 # ローカルでDockerイメージをビルドしてリモートホストへ転送
 docker build -t todoapp-client .
-docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} 'gunzip | docker load'
+docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} -p ${PORT} 'gunzip | docker load'
 
 # SSHで接続して、Dockerコンテナを実行する
 ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} -p ${PORT} "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p ${PORT}:80 todoapp-client"
