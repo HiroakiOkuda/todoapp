9fbdbcc9354f8f15c5795f90fd011aed851e4244
奥田 啓皓 <usr0104756@jinn1050.gmo.local>
2023-06-13 13:49:37 +0900

    fix: SSHクライアントがデプロイ対象ホストの公開鍵を認証できなかっ

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,12 +4,13 @@ set -e
 
 # Conoha VPSへSSH接続するキーを作成
 mkdir -p ~/.ssh
-echo "$KEY" > ~/.ssh/deploy_key && chmod 600 ~/.ssh/deploy_key
+ssh-keyscan -p ${PORT} ${HOST} >> ~/.ssh/known_hosts
+echo "${PRIVATE_KEY}" > ~/.ssh/deploy_key
+chmod 600 ~/.ssh/deploy_key
 
 # ローカルでDockerイメージをビルドしてリモートホストへ転送
 docker build -t todoapp-client .
-docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key $USERNAME@$HOST 'gunzip | docker load'
+docker save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} 'gunzip | docker load'
 
 # SSHで接続して、Dockerコンテナを実行する
-ssh-keyscan $HOST >> ~/.ssh/known_hosts
-ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/deploy_key $USERNAME@$HOST "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p $PORT:80 todoapp-client"
+ssh -i ~/.ssh/deploy_key ${USERNAME}@${HOST} -p ${PORT} "docker rm -f todoapp-client || true && docker run -d --name=todoapp-client -p ${PORT}:80 todoapp-client"
