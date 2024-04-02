70e4742899929d0b78044b8bdc35414b7bc73369
HiroakiOkuda <hiroaki-okuda@gmo.jp>
2024-04-02 14:04:44 +0900

    fix: build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,35 @@
-import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 import { MeProvider } from "@/context/me-context";
+import { FunctionComponent } from "react";
+import { NextPage } from "next";
+
+export type NextPageWithLayout<T, U> = NextPage<T> & {
+  getLayout?: (children: React.ReactElement, props: U) => React.ReactElement;
+};
+
+export type FunctionComponentWithLayout<T, U> = FunctionComponent<T> & {
+  getLayout?: (children: React.ReactElement, props: U) => React.ReactElement;
+};
 
 const client = new ApolloClient({
   uri: "/api/graphql",
   cache: new InMemoryCache(),
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout<any, any>;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <Provider store={store}>
       <ApolloProvider client={client}>
         <MeProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          {getLayout(<Component {...pageProps} />, pageProps)}
         </MeProvider>
       </ApolloProvider>
     </Provider>
