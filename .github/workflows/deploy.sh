75986d676d666dec43cdf935a13dd82ed3fc8168
HiroakiOkuda <hiroaki-okuda@gmo.jp>
2024-04-02 13:53:15 +0900

    fix: 本番用Dockerfile

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -15,15 +15,12 @@ podman build -t todoapp-client .
 podman save todoapp-client | gzip | ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" 'gunzip | podman load'
 
 # SSHで接続し、Podmanコンテナの管理
-ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" << 'EOF'
+ssh -i ~/.ssh/deploy_key "${USERNAME}@${HOST}" -p "${PORT}" << EOF
 
 # Podmanコンテナの削除と実行
-podman rm -f todoapp-client || true
-if ! podman run -d --name=todoapp-client -p 3000:3000 todoapp-client; then
-    echo "Failed to run Podman container."
-    exit 1
-fi
-
+    podman stop todoapp-client || true  # 既に実行中のコンテナがあれば停止
+    podman rm todoapp-client || true  # 停止したコンテナを削除
+    podman run -d --name todoapp-client -p 3000:3000 todoapp-client  # 新しいコンテナを起動
 EOF
 
 # エラーがあれば出力
